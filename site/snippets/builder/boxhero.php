38a04b422b5e7285435100a52f5cb3fba31c6b04
Aris <aris@papatheodorou.net>
2017-01-08 18:34:42 +0100

    “if” statement, in case we didn't choose a font…

--- site/snippets/builder/boxhero.php
+++ site/snippets/builder/boxhero.php
@@ -4,7 +4,7 @@
                     <table border="0" cellpadding="0" cellspacing="0" width="100%" height="400px"  style="margin-bottom:40px; margin-top: 20px; background-image: url('<?= $page->image($data->picture())->url() ?>'); background-repeat: no-repeat; background-size:cover !important; border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
                     <tr><td align="center" valign="middle" class="textContent" style="padding:20px;">
 
-                        <h1 style="font-family:<?= $data->text_font() ?>; font-size: 20px; line-height: 150%;color:<?= $data->text_color() ?>;  text-align: center;"> <?= $data->text() ?> </h1></a>
+                        <h1 style="<?php if ($data->text_font()->isNotEmpty()): ?>font-family:<?= $data->text_font() ?>;<?php endif ?> font-size: 20px; line-height: 150%;color:<?= $data->text_color() ?>;  text-align: center;"> <?= $data->text() ?> </h1></a>
                         <?php if ($data->link_url()->isNotEmpty()): ?>
                         <a href="<?= $data->link_url() ?>" style="color:<?= $data->text_color() ?>; text-decoration:none;">
                         <h4 style="color:<?= $data->text_color() ?>; text-decoration:none; font-weight:normal; font-size:13px; text-transform:uppercase; line-height:300%; text-align: center;">
