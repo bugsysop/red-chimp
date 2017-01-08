02e10dbf9db9d540963ff6e9815e5472095123f9
Aris <aris@papatheodorou.net>
2017-01-08 16:57:38 +0100

    Better spacing on top and bottom for “Hero Image”

--- site/snippets/builder/boxhero.php
+++ site/snippets/builder/boxhero.php
@@ -1,7 +1,7 @@
 
                 <table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
                 <tr><td align="center" valign="top" style="mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
-                    <table border="0" cellpadding="0" cellspacing="0" width="100%" height="400px"  style="margin-bottom:20px; background-image: url('<?= $page->image($data->picture())->url() ?>'); background-repeat: no-repeat; background-size:cover; border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+                    <table border="0" cellpadding="0" cellspacing="0" width="100%" height="400px"  style="margin-bottom:40px; margin-top: 20px; background-image: url('<?= $page->image($data->picture())->url() ?>'); background-repeat: no-repeat; background-size:cover !important; border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
                     <tr><td align="center" valign="middle" class="textContent" style="padding:20px;">
 
                         <h1 style="color:<?= $data->text_color() ?>; text-align: center;"> <?= $data->text() ?> </h1></a>
