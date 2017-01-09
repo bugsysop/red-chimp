96e701a3e8da1a9d45f21c3a16b3459c8abe263d
Aris <aris@papatheodorou.net>
2017-01-09 19:02:49 +0100

    Minor correction

--- site/snippets/builder/boxhero.php
+++ site/snippets/builder/boxhero.php
@@ -5,7 +5,7 @@
                     <tr><td align="center" valign="middle" class="textContent" style="padding:20px;">
 
                         <h1 style="<?php if ($data->text_font()->isNotEmpty()): ?>font-family:<?= $data->text_font() ?>;<?php endif ?> font-size: 20px; line-height: 150%;color:<?= $data->text_color() ?>;  text-align: center;"> <?= $data->text() ?> </h1></a>
-                        <?php if ($data->link_url()->isNotEmpty()): ?>
+                        <?php if ($data->link_text()->isNotEmpty()): ?>
                         <a href="<?= $data->link_url() ?>" style="color:<?= $data->text_color() ?>; text-decoration:none;">
                         <h4 style="color:<?= $data->text_color() ?>; text-decoration:none; font-weight:normal; font-size:13px; text-transform:uppercase; line-height:300%; text-align: center;">
                         <?= $data->link_text()->h() ?>&nbsp;&rarr;</h4></a>
