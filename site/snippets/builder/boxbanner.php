dec07bab138ebf18a770aa5cfc42e8a27c7c4949
Aris <aris@papatheodorou.net>
2017-01-10 08:16:11 +0100

    Finest typography for “banner” box

--- site/snippets/builder/boxbanner.php
+++ site/snippets/builder/boxbanner.php
@@ -3,10 +3,10 @@
                   <table border="0" cellpadding="0" cellspacing="0" width="100%" style="min-height=400px; margin-bottom:40px; margin-top: 20px; background-color:<?= $data->color_bg() ?>;">
                   <tr><td align="center" valign="middle" class="textContent" style="padding:20px;">
 
-                      <h1 style="<?php if ($data->text_font()->isNotEmpty()): ?>font-family:<?= $data->text_font() ?>;<?php endif ?> font-size: 20px; line-height: 150%;color:<?= $data->text_color() ?>; text-align: center;"> <?= $data->text() ?> </h1>
+                      <p style="<?php if ($data->text_font()->isNotEmpty()): ?>font-family:<?= $data->text_font() ?>;<?php endif ?> font-size: 20px; line-height: 160%;color:<?= $data->text_color() ?>; text-align: center;"> <?= $data->text() ?> </p>
                       <?php if ($data->link_text()->isNotEmpty()): ?>
-                      <h4 style="color:<?= $data->text_color() ?>; text-decoration:none; font-weight:normal; font-size:13px; text-transform:uppercase; line-height:300%; text-align: center;">
-                      <?= $data->link_text()->h() ?>&nbsp;&rarr;</h4></a>
+                      <p style="color:<?= $data->text_color() ?>; text-decoration:none; font-weight:normal; font-size:13px; text-transform:uppercase; line-height:300%; text-align: center;">
+                      <?= $data->link_text()->h() ?>&nbsp;&rarr;</p></a>
                       <?php endif ?>
 
                   </td></tr>
