32e4df5ef4e04cff6712f8afdac7bc6c09349d83
Aris <aris@papatheodorou.net>
2017-03-30 19:58:22 +0200

    Correction of #6 bug

--- site/snippets/builder/inc-letter-logo.php
+++ site/snippets/builder/inc-letter-logo.php
@@ -5,7 +5,12 @@
                     <!-- Newsletter logo -->
                     <table border="0" cellpadding="0" cellspacing="0" width="100%">
                     <tr><td class="imageContent" align="center" valign="top" style="mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
-                    <img src="<?= $page->parent()->url() . '/' . $image ?>" width="200px" class="flexibleImage" style="max-width:200px;" />
+
+                    <?php $logo = $page->parent()->letter_logo(); ?>
+                    <?php if($logo): ?>
+                    <img src="<?= $logo->resize(250)->toFile()->url() ?>" width="250" class="flexibleImage" style="max-width:250px;" />
+                    <?php endif ?>
+
                     </td></tr>
                     </table>
                     <?php endif; ?>
@@ -14,6 +19,6 @@
                     <table border="0" cellpadding="0" cellspacing="0" width="100%">
                     <tr><td class="textContent" align="center" valign="top" style="mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%; padding-bottom: 40px;">
                     <p class="" style="font-family: Helvetica,sans-serif;font-size: 12px;color: #404040;text-align:center; text-transform:uppercase; color:#999999; letter-spacing:3px;">
-                    <strong><?= $page->parent()->letter_name()->h() ?></strong> // <?= $page->letter_date('d-m-Y') ?></p>
+                    <strong><?= $page->parent()->letter_name()->h() ?></strong> // <?= $page->date('d-m-Y') ?></p>
                     </td></tr>
                     </table>
